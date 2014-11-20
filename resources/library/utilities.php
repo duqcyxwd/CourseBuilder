204ea842b6824ac634ebfd3c8f63491199eb7380
Yongqinchuan Du <duqcyxwd@gmail.com>
2014-11-20 02:07:51 -0500

    Add function getCurrentTerm and print

--- resources/library/utilities.php
+++ resources/library/utilities.php
@@ -7,5 +7,26 @@
 		header('Location: ' . $url .'?errorid=' . $statusCode);
 		exit();
 	}
+
+	function getCurrentTerm()
+	{
+		// TODO: Add functionality later
+		return "2014w";
+	}
+
+	// print array in a good way
+	function pprint($a) {
+		echo '<pre>'.print_r($a,1).'</pre>';
+	}	
+
+	function pprint2($a) {
+		foreach($a as $child) {
+			if(is_array($child)){
+				pprint2($child);
+			} else {
+		   		echo $child . "<br>";
+			}
+		}
+	}
 	
 ?>
