f7b9a99a03af5b6410777a71637a02eeb1928c34
Yongqinchuan Du <duqcyxwd@gmail.com>
2014-11-27 00:19:58 -0500

    Clean up database file and update install.php

--- resources/include/constants.php
+++ resources/include/constants.php
@@ -3,14 +3,6 @@
 
 $NUMBER_OF_YEARS = 4;
 
-$PROGRAM_NAME = array(
-												"SE" => "Software Engineering",
-												"CSE" => "Computer Systems Engineering",
-												"CE" => "Comunications Engineering",
-												"BAEE" => "Biomedical and Electrical Engineering",
-										 );
-
-
 // temporary contants
 // Will later be replaced by data pulled from database
 
--- resources/install.php
+++ resources/install.php
@@ -1,7 +1,6 @@
 <?php
-	
+	require 'Data/sqldata.php';
 	class database{
-		
 		function __construct($db){
 			$host="localhost";
 			$user="root";
@@ -37,7 +36,6 @@
 	$sql = "CREATE DATABASE IF NOT EXISTS courseBuilder" ;
 	$data->execute($sql);
 
-
 	$data = new database("courseBuilder");
 	echo "Clean old data<br>";
 
@@ -48,121 +46,19 @@
 			DROP TABLE IF EXISTS Students;
 			DROP TABLE IF EXISTS ProgramsRequirement;
 			DROP TABLE IF EXISTS Electives;
-			DROP TABLE IF EXISTS Courses;
-
-			--
-			-- Database: `courseBuilder`
-			--
-
-			-- --------------------------------------------------------
-
-			--
-			-- Table structure for table `Courses`
-			--
-
-			CREATE TABLE IF NOT EXISTS Courses(
-				Subject varchar(10)  NOT NULL,
-				CourseNumber int(4)  NOT NULL,
-				CourseTitle varchar(40)  NOT NULL,
-				PRIMARY KEY(Subject, CourseNumber)
-			);
-
-			--
-			-- Table structure for table `ProgramsRequirement`
-			--
-
-			CREATE TABLE IF NOT EXISTS ProgramsRequirement(
-				AutoId integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-				Program varchar(30) NOT NULL,
-				Subject varchar(10)  NOT NULL,
-				CourseNumber int(4)  NOT NULL,
-				YearRequirement tinyint(1),
-				FOREIGN KEY (Subject, CourseNumber) references Courses (Subject,CourseNumber)
-				-- PRIMARY KEY(Program, Subject, CourseNumber)
-			);
-
-			--
-			-- Table structure for table `Electives`
-			-- TODO set FOREIGN KEY!!!!
-			--
-
-			CREATE TABLE IF NOT EXISTS Electives(
-				AutoId integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-				ElectiveType int(4)  NOT NULL,
-				Subject varchar(10)  NOT NULL,
-				CourseNumber int(4)  NOT NULL,
-				FOREIGN KEY (Subject, CourseNumber) references Courses (Subject,CourseNumber)
-				-- PRIMARY KEY(Name, Subject, CourseNumber)
-			);
-
-			--
-			-- Table structure for table `Students`
-			--
-
-			CREATE TABLE IF NOT EXISTS Students(
-				Name varchar(30) references ProgramsRequirement (Name),
-				Email varchar(30),
-				Student_number int(9) PRIMARY KEY NOT NULL,
-				Program varchar(30) NOT NULL,
-				User_Name varchar(20) NOT NULL,
-				Password varchar(20) NOT NULL
-			);
-
-			--
-			-- Table structure for table `CourseCompleted`
-			--
-
-
-			CREATE TABLE IF NOT EXISTS CourseCompleted(
-				Student_number int(9) NOT NULL,
-				Subject varchar(10)  NOT NULL,
-				CourseNumber int(4)  NOT NULL,
-				FOREIGN KEY (Subject, CourseNumber) references Courses (Subject, CourseNumber),
-				FOREIGN KEY (Student_number) references Students (Student_number),
-				PRIMARY KEY(Student_number, Subject, CourseNumber)
-			);
-
-			--
-			-- Table structure for table `Prerequisite`
-			--
-			CREATE TABLE IF NOT EXISTS Prerequisite(
-				Subject varchar(10)  NOT NULL,
-				CourseNumber int(4)  NOT NULL,
-				Requirement varchar(200),
-				YearReq int(1),
-				FOREIGN KEY (Subject, CourseNumber) references Courses (Subject, CourseNumber)
-
-			);
-
-			--
-			-- Table structure for table `Classes`
-			--
-			CREATE TABLE IF NOT EXISTS Classes(
-				CourseId integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-				Subject varchar(10)  NOT NULL,
-				CourseNumber int(4)  NOT NULL,
-				Start_Time varchar(4),
-				End_Time varchar(4),
-				Days varchar(6),
-				RoomCap varchar(6),
-				Professor varchar(30),
-				Type varchar(4),
-				Section varchar(4),
-				Term varchar(5) NOT NULL,
-				FOREIGN KEY (Subject, CourseNumber) references Courses (Subject, CourseNumber)
-
-			);";
+			DROP TABLE IF EXISTS Courses;";
 
 	// $data->multiExe($sql);
-	if (!$data->connection->multi_query($sql)) {
-	    echo "Multi query failed: (" . $data->connection->errno . ") " . $data->connection->error;
+	// if (!$data->connection->multi_query($sql)) {
+	//     echo "Drop table failed: (" . $data->connection->errno . ") " . $data->connection->error;
+	// }
+	echo "<br>";
+
+	echo "importing data";
+	if (!$data->connection->multi_query($courseBuilderDataSql)) {
+	    echo "Drop table failed: (" . $data->connection->errno . ") " . $data->connection->error;
 	}
 	echo "<br>";
 
-
-
 	echo "done";
-
-
-	
 ?>
